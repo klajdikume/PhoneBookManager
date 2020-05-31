9283d74673eeaafd282dfb0bdc573403be868ac2
klajdikume <klajdi.kume@fshnstudent.info>
2020-05-31 08:50:36 +0200

    Added xml documentation with swagger

--- PhoneBookManager/PhoneBookManager.API/Controllers/PhoneBookController.cs
+++ PhoneBookManager/PhoneBookManager.API/Controllers/PhoneBookController.cs
@@ -32,6 +32,11 @@ namespace PhoneBookManager.API.Controllers
         //    return result;
         //}
 
+        /// <summary>
+        /// Retrieve a list of phonebooks
+        /// </summary>
+        /// <param>No parameters</param>
+        /// <returns>Return a list of ordered phonebooks</returns>
         [HttpGet]
         public ActionResult<IEnumerable<PhoneBookForReturnDTO>> Get()
         {
@@ -43,6 +48,11 @@ namespace PhoneBookManager.API.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Add a new phonebook
+        /// </summary>
+        /// <param>Http request body contains input data for a phonebook</param>
+        /// <returns>Return a list with phonebooks</returns>
         [HttpPost]
         public PhoneBookForReturnDTO Post([FromBody] PhoneBookToCreateDTO phoneBook)
         {
@@ -51,6 +61,11 @@ namespace PhoneBookManager.API.Controllers
             return result;
         }
 
+        /// <summary>
+        /// Delete a PhoneBook
+        /// </summary>
+        /// <param name="id">The ID of the desired user</param>
+        /// <returns>Return true if deleted property was set to true succesfully</returns>
         [HttpDelete("{id}")]
         public bool Delete(int id)
         {
@@ -59,6 +74,11 @@ namespace PhoneBookManager.API.Controllers
             return result;
         }
 
+        /// <summary>
+        /// Modify PhoneBook
+        /// </summary>
+        /// <param name="id">The ID of the desired user</param>
+        /// <returns>Result of modified user</returns>
         [HttpPut("{id}")]
         public ActionResult<PhoneBookForReturnDTO> Put(int id, [FromBody] PhoneBookToCreateDTO phoneBook)
         {
@@ -72,6 +92,11 @@ namespace PhoneBookManager.API.Controllers
             return result;
         }
 
+        /// <summary>
+        /// Retrieve the phoneBook by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the desired user</param>
+        /// <returns>an user entity with a list of numbers</returns>
         [HttpGet("{id}")]
         public ActionResult<PhoneBookForReturnDTO> Get(int id)
         {
